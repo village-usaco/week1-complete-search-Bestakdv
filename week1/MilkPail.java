fd2e52663e44794cff2f30698b1d44ff4c49c90e
Bestakdv <86662969+Bestakdv@users.noreply.github.com>
2025-04-16 15:01:58 -0500

    Update MilkPail.java

--- week1/MilkPail.java
+++ week1/MilkPail.java
@@ -1,14 +1,38 @@
 package usaco;
 
-/**
- *
- * @author 
- */
 public class MilkPail
 {
-  public static void main(String[] args){
+
+  public int pails(int X, int Y, int M)
+  {
+    int max = 0;
+    if(max > X && max > Y)
+      return max;
+
+    int amount = M % X;
+    if(amount != M)
+    {
+      
+      for(int i=0; i<amount;i++)
+      {
+        max+=X;
+      }
+    }
+    int otherMax = max;
+    int otherAmount = M % Y;
+    if(otherAmount != M)
+    {
+
     
-    
+      for(int i=0; i<otherAmount; i++)
+      {
+        max+=Y;
+      }
+    }
+    return max;
   }
-  
+  public static void main(String[] args)
+  {
+    pails(17,25,77);
+  }
 }
