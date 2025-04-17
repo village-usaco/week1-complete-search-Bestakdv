a19dda2535586f488c2c6974869a976ef089c391
Bestakdv <86662969+Bestakdv@users.noreply.github.com>
2025-04-16 19:17:18 -0500

    Update MilkPail.java

--- week1/MilkPail.java
+++ week1/MilkPail.java
@@ -13,7 +13,6 @@ public class MilkPail
     int amount = M % X;
     if(amount != M)
     {
-      
       for(int i=0; i<amount;i++)
       {
         max+=X;
@@ -30,13 +29,19 @@ public class MilkPail
     }
     return max;
   }
+
   public static void main(String[] args)
   {
     Scanner kb = new Scanner(System.in);
+    System.out.print("Enter X: ");
     int X = kb.nextInt();
+    System.out.print("Enter Y: ");
     int Y = kb.nextInt();
+    System.out.print("Enter M: ");
     int M = kb.nextInt();
+    
     MilkPail test = new MilkPail();
     int result = test.pails(X,Y,M);
+    System.out.println("Output: " + result);
   }
 }
